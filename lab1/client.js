f4a40a32e9e475abac4f1730fdc20d1afa693d93
wilhelm <wilhelmvonka@outlook.com>
2022-12-22 20:44:20 +0100

    Played around with the assignment a bit

--- lab1/client.js
+++ lab1/client.js
@@ -1,7 +1,22 @@
-display = function() {
+let displayNotLoggedin = function() {
     //something
 }
+let displayLoggedin = function() {
+    //something
+}
+
+function logIn(){
+    document.getElementById("not-logged-in-wrapper").style.display = "none";
+    document.getElementById("logged-in-wrapper").style.display = "flex";
+}
+
+function logOut(){
+    document.getElementById("logged-in-wrapper").style.display = "none";
+    document.getElementById("not-logged-in-wrapper").style.display = "flex";
+}
+
 window.onload = function(){
     console.log("page has been loaded");
+    displayNotLoggedin();
 }
 
