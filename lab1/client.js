119592c40775e0ec3b88623646cdc96380a921cc
wilhelm <wilhelmvonka@outlook.com>
2022-12-29 16:37:19 +0100

    progess with the error modal

--- lab1/client.js
+++ lab1/client.js
@@ -2,53 +2,35 @@ let displayNotLoggedin = function() {
     let welcomeView = document.getElementById("welcome-view-container");
     let welcomeSection = document.getElementById("welcome-section");
     welcomeSection.innerHTML = welcomeView.innerHTML;
-
-    // welcomeSection.children[0].style.position = "relative";
-    // welcomeSection.children[0].style.top = "50%";
-    // welcomeSection.children[0].style.left = "50%";
-    // welcomeSection.children[0].style.transform = "translate(-50%, -50%)";
-
-    // centerElementsInWindow("welcome-view");
-    // centerElementsInWindow("error-modal");
-    // document.getElementById("welcome-section").style.zIndex = 1;
-    // document.getElementById("sign-up-error-modal").style.zIndex = 9;
 }
 let displayLoggedin = function() {
     //something
 }
 
-function validateSignUp(){
-    let formElement = document.getElementById("sign-up-form");
+function validateSignUp(event){
     let genderSelect = document.getElementById("gender-input");
     let password = document.getElementById("password-new");
     let passwordRepeat = document.getElementById("password-repeat");
     
-
-    // Om input är fel (i någon if-sats) 
-    // 1. Göm inte welcome-section, det är konstig design. Se till att en ruta
-    //    kommer upp ovanför allt men att det syns i bakgrunden
-    // 2. visa error modal med lämplig text
-
-    let errorModal = document.getElementById("sign-up-error-modal");
-    // document.getElementById("welcome-section").style.display = "none";
-    console.log(errorModal);
-    centerElementsInWindow("sign-up-error-modal");
-    errorModal.style.display = "flex";
-    
+    let modalTitle = "Some info is missing";
     if (genderSelect.value == "NO_CHOICE"){
+        let modalBody = [
+                            "You have to pick a gender.",
+                        ]
+        showModal("welcome-section", modalBody, modalTitle);
+        event.preventDefault();
         return false;
     }
     
     if (password != passwordRepeat){
+        let modalBody = [
+                            "The passwords are not equal to each other.",
+                        ]
+        showModal("welcome-section", modalBody, modalTitle);
+        event.preventDefault();
         return false;
     }
-    
-    
-    // console.log(formElement);
-    // console.log(genderSelect.value); 
-    // console.log(password.value); 
-    // console.log(passwordRepeat.value); 
-    // event.preventDefault();
+    return true;
 }
 
 function centerElementsInWindow(currentView){
@@ -74,17 +56,29 @@ function putDotsBetweenLabelInputPair(){
     }
 }
 
-function showModal(section){
+function showModal(section, modalBody, modalTitle){
+    console.log("inside Showmodal");
     let modal = document.getElementById("error-modal");
-    let overlay = document.getElementById("error-overlay");
     let currentSection = document.getElementById(section);
+    
+    let currentModalBody = document.getElementById("modal-body");
+    let currentModalTitle = document.getElementById("modal-title");
+    modalBody.forEach(element => {
+        currentModalBody.innerHTML += element;
+    });
+    currentModalTitle.innerHTML += modalTitle;
+
+
     currentSection.style.pointerEvents = "none";
     modal.style.transform += "translate(-50%, -50%) scale(1.0)";
-    overlay.style.transform = "scale(1.0)";
 }
 
 function closeModal(section){
-    console.log("hej!");
+    let modal = document.getElementById("error-modal");
+    let currentSection = document.getElementById(section);
+    currentSection.style.pointerEvents = "auto";
+    modal.style.transform += "scale(0)";
+    location.reload();
 }
 
 function logIn(){
