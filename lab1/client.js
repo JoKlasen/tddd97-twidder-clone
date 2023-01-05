fa26c2d8985560244c5af114f26ebb952d96eb33
wilvo970 <wilvo970@student.liu.se>
2023-01-05 15:46:40 +0100

    some progress with the automatic sign in

--- lab1/client.js
+++ lab1/client.js
@@ -1,6 +1,29 @@
-function closeWelcomeSection(){
-    let welcomeSection = document.getElementById("welcome-section");
-    welcomeSection.innerHTML = '';
+function setCookie(token){
+    let currentDate = new Date();
+    document.cookie = 'logged_in_user=' + token + '; expires=' + currentDate.toString + '; path=/';
+}
+
+// Returns empty string if cookieName didn't match any saved cookie
+function getCookie(cookieName){
+    let cookies = document.cookie.split(';');
+    console.log('Cookies: ');
+    console.log(cookies);
+
+    let data = '';
+    
+    for (let i = 0; i < cookies.length; i++){
+        if (cookies[i].includes(cookieName)){
+            let equalSign = cookies[i].indexOf('=');
+            data = cookies[i].substring(equalSign, cookies[i].length - 1);
+            console.log('token: ');
+            console.log(data);
+            break;
+        }
+    }
+}
+
+function activeUser(){
+    return serverstub.getUserDataByToken(getCookie('logged_in_user')) !== null;
 }
 
 function putDotsBetweenLabelInputPair(){
@@ -48,10 +71,11 @@ function validateSignIn(event){
         return false;
     }
 
-    // Might be redundant
-    window.localStorage.setItem('sign-in-status', 'success');
-    window.localStorage.setItem('sign-in-message', response.message);
-    document.cookie
+    setCookie(response.data);
+    displayView();
+
+    // No refresh of the webpage. Profile view loaded manually.
+    event.preventDefault();
 }
 
 function validateSignUp(event){
@@ -101,13 +125,6 @@ function validateSignUp(event){
     return true;
 }
 
-function centerElementsInWindow(currentView){
-    let wrapperDiv = document.getElementById(currentView);
-    let center = window.innerWidth / 2;
-    let offset = center - (wrapperDiv.offsetWidth / 2); 
-    wrapperDiv.style.marginLeft = offset;
-}
-
 function showModal(section, modalBody, modalTitle){
     console.log("inside Showmodal");
     let modal = document.getElementById("error-modal");
@@ -134,7 +151,6 @@ function closeModal(section){
     modal.style.transform = "translate(-50%, -50%) scale(0)";
 }
 
-
 function checkSignUpStatus(){
     
     if (window.localStorage.getItem('sign-up-status') === null)
@@ -145,64 +161,71 @@ function checkSignUpStatus(){
     
     let modalTitle = 'Sign up status: ' + window.localStorage.getItem('sign-up-status');
     let modalBody = [
-                        window.localStorage.getItem('sign-up-message')
-                    ]
-                    
+        window.localStorage.getItem('sign-up-message')
+    ]
+    
     showModal(  'welcome-section',  
                 modalBody,
                 modalTitle
-             )
-
+                )
+                
     window.localStorage.removeItem('sign-up-status');
     window.localStorage.removeItem('sign-up-message'); 
-}
-        
-// function activeToken(){
-//     window.localStorage.setItem('alreadyActive', 'true');
-//     document.cookie = '';
-// }
-
-function checkSignInStatus(){
-    if (window.localStorage.getItem('sign-in-status') === null)
-    {
-        console.log("No sign in was done previous this refresh");
-        return;
-    }
-
-    let status = window.localStorage.getItem('sign-in-status');
-    if (status === 'success'){
-        // activeToken();
-        closeWelcomeSection();
-        displayLoggedin();
-    } else {
-        let modalTitle = 'Sign in status: ' + status;
-        let modalBody = [
-                            window.localStorage.getItem('sign-in-message')
-                        ]
     
-        showModal(  'welcome-section',  
-                    modalBody,
-                    modalTitle
-                 )
-    }
-    
-    window.localStorage.removeItem('sign-in-status');
-    window.localStorage.removeItem('sign-in-message'); 
+    return;
 }
 
-function handlePreviousSession(){
-    checkSignUpStatus();
-    checkSignInStatus();
-}
-   
 function displayView(){
-    let isLoggedIn = window.localStorage.getItem('session-status');
-    displayNotLoggedin();
+    // Might need information about who is logged in. Future problem
+    let loggedIn = activeUser(); 
+
+    //if (loggedIn){
+    //    displayLoggedin();
+    //} else{
+        displayNotLoggedin();
+    //}
 }
 
 window.onload = function(){
     console.log("page has been loaded");
     displayView();
-    handlePreviousSession();
+    checkSignUpStatus();
 }
 
+
+
+
+
+
+
+
+// function checkSignInStatus(){
+//     if (window.localStorage.getItem('sign-in-status') === null)
+//     {
+//         console.log("No sign in was done previous this refresh");
+//         // throw new Error('No sign in was done previous this refresh');
+//         return;
+//     }
+    
+//     let status = window.localStorage.getItem('sign-in-status');
+//     if (status === 'success'){
+//         // activeToken();
+//         closeWelcomeSection();
+//         displayLoggedin();
+//     } else {
+//         let modalTitle = 'Sign in status: ' + status;
+//         let modalBody = [
+//             window.localStorage.getItem('sign-in-message')
+//         ]
+        
+//         showModal(  'welcome-section',  
+//         modalBody,
+//         modalTitle
+//         )
+//     }
+    
+//     window.localStorage.removeItem('sign-in-status');
+//     window.localStorage.removeItem('sign-in-message'); 
+    
+//     return true;
+// }
